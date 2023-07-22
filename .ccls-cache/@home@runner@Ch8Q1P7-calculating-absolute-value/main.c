52033737fbfcfb92d14ddefcd3e229639a2e4c42
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-22 13:32:47 +0000

    copied program

--- .ccls-cache/@home@runner@Ch8Q1P7-calculating-absolute-value/main.c
+++ .ccls-cache/@home@runner@Ch8Q1P7-calculating-absolute-value/main.c
@@ -1,6 +1,41 @@
+// Function to calculate the absolute value
+
 #include <stdio.h>
 
+// Declare functions
+float absoluteValue (float);
+
 int main(void) {
-  printf("Hello World\n");
+
+  // Define and declare input
+  float f1 = -15.5, f2 = 20.0, f3 = -5.0;
+  int i1 = -716;
+  float result;
+
+  // Use functions
+  result = absoluteValue (f1);
+  printf ("Result = %.2f\n", result);
+  printf ("f1 = %.2f\n", f1);
+
+  result = absoluteValue (f2) + absoluteValue (f3);
+  printf ("Result = %.2f\n", result);
+
+  result = absoluteValue ((float) i1);
+  printf ("Result = %.2f\n", result);
+
+  result = absoluteValue (i1);
+  printf ("Result = %.2f\n", result);
+
+  printf ("%.2f\n", absoluteValue (-6.0) / 4);
+  
+  
   return 0;
+}
+
+// Define functions
+float absoluteValue (float input) {
+  if (input < 0)
+    input *= -1;
+
+  return input;
 }
